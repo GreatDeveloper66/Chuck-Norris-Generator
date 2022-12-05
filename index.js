a827805b4ff41182e49979546711dfa87f5c7247
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-04 19:34:04 -0600

    add require dotenv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+const fetch = require('node-fetch');
 
 app.use(express.static('public'))
 
 app.listen(port, () => {
     console.log(`app is listening at http://localhost:${port}`)
-    
 })
+
