27130408e9528cb47eea4cc4b73ca53245d8f5e9
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-04 19:56:38 -0600

    Add env variables and fetch lines

--- index.js
+++ index.js
@@ -3,10 +3,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fetch = require('node-fetch');
+const api_key = process.env.X-RapidAPI-Key
+const api_host = proces.env.X-RapidAPI-Host
 
 app.use(express.static('public'))
+
 
 app.listen(port, () => {
     console.log(`app is listening at http://localhost:${port}`)
 })
 
+app.get('/chucknorrisrandomjoke', async (request, response) => {
+
+})
+
+const url = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random';
+
+const options = {
+    method: 'GET',
+    headers: {
+    accept: 'application/json',
+    'X-RapidAPI-Key': api_key,
+    'X-RapidAPI-Host': api_host
+    }
+    };
+
+fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));
