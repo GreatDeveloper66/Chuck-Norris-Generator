03cbedb918a3937300c0789743bdbf6cfcd6b3e5
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-09 23:45:08 -0600

    Add fetch code in get route

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ import fetch from 'node-fetch'
 const api_key = process.env.XRapidAPIKey
 const api_host = process.env.XRapidAPIHost
 
-app.use(express.static('public'))
-
-app.listen(port, () => {
-    console.log(`app is listening at http://localhost:${port}`)
-})
-
-app.get('/chucknorrisrandomjoke', async (request, response) => {
-
-})
-
 const url = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random';
 
 const options = {
@@ -30,7 +20,21 @@ const options = {
     }
     };
 
-fetch(url, options)
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`app is listening at http://localhost:${port}`)
+})
+
+app.get('/chucknorrisrandomjoke', async (request, response) => {
+    const fetchAPI = await fetch(url, options)
     .then(res => res.json())
     .then(json => console.log(json))
     .catch(err => console.error('error:' + err));
+    const fetchResponse = await fetchAPI.json()
+    response.json(fetchResponse)
+})
+
+
+
+
