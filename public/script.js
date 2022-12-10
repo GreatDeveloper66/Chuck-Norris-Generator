0ab03a72219b26fa164ccff1304391cd127da04e
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-10 00:10:48 -0600

    Add call to chuck norris route from script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,5 @@
 console.log('script.js loaded')
+async function getChuckNorrisQuote() {
+    const response = await fetch('/chucknorrisrandomjoke')
+    const jsonResponse = await response.json()
+}
