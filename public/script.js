9029a44f3efaa90758628572227073b2d536de7f
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-10 00:11:27 -0600

    Add call to getChuckNorrisQuote

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 console.log('script.js loaded')
+getChuckNorrisQuote()
 async function getChuckNorrisQuote() {
     const response = await fetch('/chucknorrisrandomjoke')
     const jsonResponse = await response.json()
