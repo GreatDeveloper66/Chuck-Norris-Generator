1978ce5c2cb49a3a5be6c2d11fecec05b29d0e65
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-10 18:02:08 -0600

    add js code to insert chuck norris quote into paragraph

--- public/script.js
+++ public/script.js
@@ -3,4 +3,5 @@ getChuckNorrisQuote()
 async function getChuckNorrisQuote() {
     const response = await fetch('/chucknorrisrandomjoke')
     const jsonResponse = await response.json()
+    document.getElementById("chuckNorrisQuote").innerHTML = jsonResponse.value
 }
