2ebbeccccbabf8b7959180b3dd570c863fab994a
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-12 00:02:15 -0600

    Change get request to async function

--- public/script.js
+++ public/script.js
@@ -1,15 +1,11 @@
 console.log('script.js loaded')
 
-fetch('/chucknorrisrandomjoke').
-    then(res => res.json()).
-    then((data) => console.log("data: " + data)).
-    catch(err => console.log(`this is an error: {err}`))
 
-/*
+
+
 getChuckNorrisQuote()
 async function getChuckNorrisQuote() {
     const response = await fetch('/chucknorrisrandomjoke')
     const jsonResponse = await response.json()
     document.getElementById("chuckNorrisQuote").innerHTML = jsonResponse.value
 }
-*/
