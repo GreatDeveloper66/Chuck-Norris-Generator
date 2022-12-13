56323304ee577ff43405712623dde160c6024dd8
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-12 21:53:52 -0600

    Set up message variables from json response

--- public/script.js
+++ public/script.js
@@ -7,5 +7,9 @@ getChuckNorrisQuote()
 async function getChuckNorrisQuote() {
     const response = await fetch('/chucknorrisrandomjoke')
     const jsonResponse = await response.json()
+    console.log(jsonResponse)
+    const fact = jsonResponse.value
+    const imgUrl = jsonResponse.icon_url
+    
     document.getElementById("chuckNorrisQuote").innerHTML = jsonResponse.value
 }
