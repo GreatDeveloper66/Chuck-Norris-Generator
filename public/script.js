e5ba58e2fe27bab153c939c0f2ecc508b8c3c3b6
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-12 22:21:22 -0600

    add refresh button

--- public/script.js
+++ public/script.js
@@ -3,13 +3,26 @@ console.log('script.js loaded')
 
 
 
-getChuckNorrisQuote()
+
+console.log(document.getElementById("chuckNorrisQuote"))
+
 async function getChuckNorrisQuote() {
     const response = await fetch('/chucknorrisrandomjoke')
     const jsonResponse = await response.json()
     console.log(jsonResponse)
     const fact = jsonResponse.value
     const imgUrl = jsonResponse.icon_url
-    
-    document.getElementById("chuckNorrisQuote").innerHTML = jsonResponse.value
+
+    document.getElementById("chuckNorrisQuote").innerHTML = fact
+    document.getElementById("chuckNorrisImage").getAttributeNode("src").value = imgUrl
 }
+
+window.addEventListener("load", event => {
+    getChuckNorrisQuote()
+    document.getElementById("refreshButton").addEventListener("click", () => {
+        getChuckNorrisQuote()
+    })
+
+})
+
+
