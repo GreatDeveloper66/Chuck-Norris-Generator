cec68e2241a7cb554d4c172fa96c6129ffd9b87a
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-17 23:41:34 -0600

    organize index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-
+/*import*/
 import * as dotenv from 'dotenv'
 dotenv.config()
-
 import express from 'express'
+import fetch from 'node-fetch'
+
+/*constants*/
 const app = express()
 const port = 3000
-import fetch from 'node-fetch'
 const api_key = process.env.XRapidAPIKey
 const api_host = process.env.XRapidAPIHost
-
 const url = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random';
-
 const options = {
   method: 'GET',
   headers: {
@@ -20,16 +19,14 @@ const options = {
   }
 };
 
-
-
-
-
+/*set up server*/
 app.use(express.static('public'))
 
 app.listen(port, () => {
     console.log(`app is listening at http://localhost:${port}`)
 })
 
+/*set up get routes*/
 app.get('/chucknorrisrandomjoke', async (request, response) => {
    const fetchAPI = await fetch(url,options)
    const chuckNorrisResponse = await fetchAPI.json()
